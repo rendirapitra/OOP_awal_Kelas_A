cce0cabddee47a29dab8a7649788a8c74e571702
rendirapitra <148045246+rendirapitra@users.noreply.github.com>
2023-11-21 12:03:01 +0700

    RendiRapitra.php

--- RendiRapitra.php
+++ RendiRapitra.php
@@ -1,43 +1,60 @@
+<!DOCTYPE html>
+<html>
+<body>
 <?php
-class mahasiswa
-{
-    // Properties
-    private $name;
-    private $nim;
-    private $matkul;
-    // Methods
-    function set_name($name)
-    {
-        $this->name = $name;
-    }
-    function get_name()
-    {
-        return $this->name;
-    }
-    function set_nim($nim)
-    {
-        $this->nim = $nim;
-    }
-    function get_nim()
-    {
-        return $this->nim;
-    }
-    function set_matkul($matkul)
-    {
-        $this->matkul = $matkul;
-    }
-    function get_matkul()
-    {
-        return $this->matkul;
-    }
+class mahasiswa {
+  // Properties
+  private $name;
+  private $nim;
+  private $matkul;
+  private $cita_cita;
+
+  // Methods
+  function set_name($name) {
+    $this->name = $name;
+  function __construct ($name, $nim, $matkul, $cita_cita){
+  	$this->name = $name;
+    $this->nim = $nim;
+    $this->matkul = $matkul;
+    $this->cita_cita = $cita_cita;
+  }
+  function get_name() {
+    return $this->name;
+  }
+  function set_nim($nim) {
+    $this->nim = $nim;
+  }
+  function get_nim() {
+    return $this->nim;
+  }
+  function set_matkul($matkul) {
+  	$this->matkul = $matkul;
+  }
+  function get_matkul () {
+  function get_matkul() {
+  	return $this->matkul;
+  }
+  function get_cita_cita() {
+  	return $this->cita_cita;
+  }
 }
 
 $Rendi = new mahasiswa();
 $Rendi->set_name('Rendi Rapitra');
 $Rendi->set_nim('2255201029');
 $Rendi->set_matkul('pbo');
-echo "Name: " . $Rendi->get_name();
-echo "\n";
-echo "nim: " . $Rendi->get_nim();
-echo "\n";
+echo "Nama: " . $Rendi->get_name();
+$Rendi = new mahasiswa('Rendi Rapitra', '2255201029', 'pbo', 'Main Bola');
+echo $Rendi->get_name();
+echo "<br>";
+echo $Rendi->get_nim();
+echo "<br>";
+echo "nim: " .  $Rendi->get_nim();
+echo $Rendi->get_matkul();
+echo "<br>";
 echo "matkul: " . $Rendi->get_matkul();
+echo $Rendi->get_cita_cita();
+
+?>
+
+</body>
