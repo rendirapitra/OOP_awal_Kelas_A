348b29c8f61e7e3faabacc6f12c65f60cbfe1bd0
rendirapitra <148045246+rendirapitra@users.noreply.github.com>
2023-11-23 10:29:57 +0700

    RendiRapitra.php

--- RendiRapitra.php
+++ RendiRapitra.php
@@ -20,20 +20,11 @@ class mahasiswa {
   function get_name() {
     return $this->name;
   }
-  function set_nim($nim) {
-    $this->nim = $nim;
-  }
   function get_nim() {
     return $this->nim;
   }
-  function set_matkul($matkul) {
-  	$this->matkul = $matkul;
-  }
   function get_matkul() {
   	return $this->matkul;
-  }
-  function set_cita_cita() {
-    $this->cita_cita = $cita_cita;
   }
   function get_cita_cita() {
   	return $this->cita_cita;
@@ -41,13 +32,13 @@ class mahasiswa {
 }
 
 $Rendi = new mahasiswa('Rendi Rapitra', '2255201029', 'pbo', 'Main Bola');
-echo "Nama: " . $Rendi->get_name();
+echo $Rendi->get_name();
 echo "<br>";
-echo "NIM: " . $Rendi->get_nim();
+echo $Rendi->get_nim();
 echo "<br>";
-echo "Matkul: " .  $Rendi->get_matkul();
+echo $Rendi->get_matkul();
 echo "<br>";
-echo "Cita-Cita: " . $Rendi->get_cita_cita();
+echo $Rendi->get_cita_cita();
 
 ?>
 
