c560645bde19238fd609ce10dbdde162549de570
rendirapitra <148045246+rendirapitra@users.noreply.github.com>
2023-11-28 09:01:02 +0700

    RendiRapitra.php

--- RendiRapitra.php
+++ RendiRapitra.php
@@ -29,16 +29,27 @@ class mahasiswa {
   function get_cita_cita() {
   	return $this->cita_cita;
   }
+  function __destruct() {
+    echo "<br> Ini akhir dari class mahasiswa {$this->name}";
+  }
 }
-
+  class organisasi extends mahasiswa 
+    {
+      function hima
+    {
+      echo $Rendi->get_name();
+  echo "<br>";
+  echo $Rendi->get_nim();
+  echo "<br>";
+  echo $Rendi->get_matkul();
+  echo "<br>";
+  echo $Rendi->get_cita_cita();
+  echo "<br>";
+  echo "Organisasi: Hima";
+    }
+}
 $Rendi = new mahasiswa('Rendi Rapitra', '2255201029', 'pbo', 'Main Bola');
-echo $Rendi->get_name();
-echo "<br>";
-echo $Rendi->get_nim();
-echo "<br>";
-echo $Rendi->get_matkul();
-echo "<br>";
-echo $Rendi->get_cita_cita();
+$Rendi->hima();
 
 ?>
 
